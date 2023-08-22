ff4cc58c24f24d520465247331c0f174712eb808
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-08-22 16:40:07 +0000

    Add StatefulGreetingWithCallback

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="Im a stateful class component" name="Thomas" />
+      <StatefulGreetingWithCallback />
     </div>
   );
 }
