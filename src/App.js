b6103b86c4cf74f40d8a08c9336cae16c87a5bce
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-08-22 17:58:38 +0000

    Add Events components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
+
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreetingWithPrevState />
+        <EventsFunctional />
+        <EventsClass />
     </div>
   );
 }
